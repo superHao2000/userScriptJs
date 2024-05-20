d1f543b681ca4597f343513e30d541240834e26e
superhao <2217745768@qq.com>
2024-05-20 11:24:55 +0800

    fix: 替换ck争取匹配当前主域名

--- user-cookie.user.js
+++ user-cookie.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name        一键ck获取替换
 // @namespace   Violentmonkey Scripts
+// @description 获取当前页面的cookie的字符串或者使用cookie字符串替换当前页面cookie（注意HttpOnly需要手动取消）
 // @match       *://*/*
 // @grant       GM_registerMenuCommand
 // @grant       GM_notification
 // @grant       GM_setClipboard
-// @version     1.0
-// @author      superhao
+// @version     1.1
+// @author      superHao
 // @description 2024/5/13 10:31:52
 // ==/UserScript==
 
@@ -14,32 +15,41 @@
  * 获取当前页面cookie
  */
 function getCookie() {
-    cookies = document.cookie
-    return cookies
+    return document.cookie
 }
 /**
-* 将字符串cookie替换为数组cookie
-* @cookieString cookie字符串
-*/
+ * 获取当前页面主域名
+ */
+function getHost() {
+    let host = window.location.host;
+    let host1 = host.slice(0,host.indexOf('.'));
+    let host2 = host.slice(host.indexOf('.')+1);
+    return host2
+}
+/**
+ * 将字符串cookie替换为数组cookie
+ * @cookieString cookie字符串
+ */
 function parseCookieString(cookieString) {
     // cookiesString.split(/\s*;\s*/).map(it => it.split('='))[0]
-    cookies=cookieString.split(";").map(e=>e.trim())
-    return cookies
+    return cookieString.split(";").map(e => e.trim());
 }
+
 GM_registerMenuCommand(
     "获取cookie",
     function () {
-        cookie = getCookie()
-        GM_setClipboard(cookie)
-        GM_notification("cookie复制成功")
+        let cookie = getCookie();
+        GM_setClipboard(cookie,"text");
+        GM_notification("cookie复制成功");
     });
 GM_registerMenuCommand(
     "替换cookie",
     function () {
-        cookies = prompt("请输入cookie：")
-        cookies=parseCookieString(cookies)
+        let cookies = prompt("请输入cookie：");
+        cookies = parseCookieString(cookies);
+        let domain=getHost();
         cookies.forEach(element => {
-            document.cookie=element+";domain=.bilibili.com;path=/;"
+            document.cookie = element + ";domain=."+domain+";path=/;"
         });
         location.reload();
     }
